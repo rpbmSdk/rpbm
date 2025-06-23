825052b781314dfdf88b609f58cd3477592d1c6d
Stanley Serin <contact@p0si.net>
2025-06-23 11:40:12 +0200

    Update version and dependencies in __manifest__.py for release 17.0.250623.1

--- rpbm_agent/__manifest__.py
+++ rpbm_agent/__manifest__.py
@@ -1,10 +1,10 @@
 {
     "name": "RPBM Agent",
-    "version": "17.0.250219.0",
+    "version": "17.0.250623.1",
     "category": "Tools",
     "description": """""",
     "author": "Paradigme SASU",
-    "depends": ["crm", "fleet"],
+    "depends": ["crm", "fleet", "sale_crm"],
     'assets':{
         'web.assets_backend': [
             'rpbm_agent/static/src/*',
